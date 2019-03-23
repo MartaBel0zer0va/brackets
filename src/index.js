45d33eac930b2e9b2e832f11ad210807d6149e68
Margarita <>
2019-03-24 01:48:01 +0300

    f

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str == 0)
+      return true;
+  if (str == 1)
+      return false;
+  if (bracketsConfig[0] == ')' || bracketsConfig[0] == ']' || bracketsConfig[0] =='}')
+      return false;
+  var count = check(bracketsConfig[0]);
+var j;
+var counter = 0;
+
+if (j == str )
+    return false;
+if (j == 1)
+    return check(str - 2, bracketsConfig + 2);
+return check(j - 1, bracketsConfig + 1) && check(str - j - 1, bracketsConfig + j + 1);
+}
+
+function ClosingBracket(n) {
+    if (n == '(')
+        return ')';
+    if (n == '[')
+        return ']';
+    if (n == '{')
+        return '}';
+    return -1;
 }
