bef8b49a7c753141f859d21c4c2f5bc708a2de42
Margarita <>
2019-03-24 15:04:45 +0300

    s

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str == 0)
+    if (str === 0)
       return true;
-  if (str == 1)
+  if (str === 1)
       return false;
-  if (bracketsConfig[0] == ')' || bracketsConfig[0] == ']' || bracketsConfig[0] =='}')
+  bracketsConfig = {};
+  var firstEl = bracketsConfig[0];
+  if (firstEl == ')' || firstEl == ']' || firstEl =='}')
       return false;
   var count = check(bracketsConfig[0]);
 var j;
 var counter = 0;
-for (j = 1, j < str, j++) {
-    if (bracketsConfig[j] == bracketsConfig[0])
+for (j = 1; j < str; j++) {
+    if (bracketsConfig[j] === bracketsConfig[0])
         counter++;
-    if (bracketsConfig[j] == count) {
-        if (counter == 0)
+    if (bracketsConfig[j] === count) {
+        if (counter === 0)
             break;
         counter--;
     }
 }
-if (j == str )
+if (j === str )
     return false;
-if (j == 1)
+if (j === 1)
     return check(str - 2, bracketsConfig + 2);
 return check(j - 1, bracketsConfig + 1) && check(str - j - 1, bracketsConfig + j + 1);
 }
@@ -32,4 +34,10 @@ function ClosingBracket(n) {
     if (n == '{')
         return '}';
     return -1;
-}
+}/*
+let bracketsConfig = ['[(]'];
+var str = bracketsConfig[].length;
+if (check(str, bracketsConfig))
+     console.log('balanced');
+else console.log('not balanced');
+return;*/
